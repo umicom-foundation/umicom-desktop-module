b5b0de44c8949fedea79354e6e8e8e8326b5ec95
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-08-30 22:42:40 +0100

    feat(desktop): enhance productisation contribution

--- include/umicom/desktop_module/productisation_contribution.h
+++ include/umicom/desktop_module/productisation_contribution.h
@@ -16,7 +16,7 @@
 #ifndef UMICOM_DESKTOP_MODULE_PRODUCTISATION_CONTRIBUTION_H
 #define UMICOM_DESKTOP_MODULE_PRODUCTISATION_CONTRIBUTION_H
 
-#include "umicom/application/productisation/adoption.h"
+#include "umicom/application/productisation/session.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +26,8 @@ const UmiProductApplicationAdoption *
 umi_desktop_module_productisation_contribution(void);
 UmiStatus umi_desktop_module_productisation_snapshot(
     UmiProductApplicationAdoptionSnapshot *out_snapshot);
+UmiStatus umi_desktop_module_product_session_init(
+    UmiProductApplicationSession *out_session);
 
 #ifdef __cplusplus
 }
